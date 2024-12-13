79923bc143e93eb52752deaca05165d9dd777963
shunii <r05i36@ed.cc.suzuka-ct.ac.jp>
2024-12-13 20:03:25 +0900

    feat: could save image on web platform

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import * as ImagePicker from "expo-image-picker"
 import * as MediaLibrary from "expo-media-library"
-import { StyleSheet, Text, View } from "react-native"
+import { Platform, StyleSheet, Text, View } from "react-native"
 import { ImageViewer } from "@/components/ImageViewer"
 import { Button } from "@/components/Button"
 import { useRef, useState } from "react"
@@ -12,6 +12,7 @@ import { EmojiList } from "@/components/EmojiList"
 import { EmojiSticker } from "@/components/EmojiSticker"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import { captureRef } from "react-native-view-shot"
+import domtoimage from "dom-to-image"
 
 const PlaceholderImage = require("@/assets/images/background-image.png")
 
@@ -56,17 +57,35 @@ export default function Index() {
   }
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      })
-      await MediaLibrary.saveToLibraryAsync(localUri)
-      if (localUri) {
-        alert("Saved!")
+    if (Platform.OS !== "web") {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        })
+        await MediaLibrary.saveToLibraryAsync(localUri)
+        if (localUri) {
+          alert("Saved!")
+        }
+      } catch (e) {
+        console.log(e)
       }
-    } catch (e) {
-      console.log(e)
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        })
+
+        let link = document.createElement("a")
+        link.download = "sticker-smash.jpeg"
+        link.href = dataUrl
+        link.click()
+        link.remove()
+      } catch (e) {
+        console.log(e)
+      }
     }
   }
 
